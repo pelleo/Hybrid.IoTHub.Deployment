1af3cde95c2dafb6d37514a52ab08feaf5d53007
Pelle Olsson <pelleo@microsoft.com>
2021-12-01 14:29:49 +0100

    Fixed typo

--- scripts/create-vm.sh
+++ scripts/create-vm.sh
@@ -17,14 +17,13 @@ networkInterfaceName=${vmName}-nic   # vm-infra.bicep
 sshRSAPublicKey='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsjYzxGD3DuHdin5WShA4/GMF53+0QVjCsV9dJgXrt2INF5T8LX+Gu7tFXHcKOhqkoRzqNC+jYGdEkUFqNmKtZZ0S/DflUVt+DvM7jukY/++f57UZdw1mWDtxxCK5CYg5tOzAJQC7h9YhUxaXUOTJ/uFQvm5628sIR3Id27qarV07oi56gJyD6/6AVBQWsthB8Qwif6KQdHHzH0ZW1AF5W1HVg0OGgFBsiFLQx6uQGCCQGSiyjPsM6s0UqlTvbiXbrZ0LHj+DGQp6leeZghblOw4O5jYWfIBgO1+ioVToc0U8TRuQCqerueLDH9NZxObRBpA53NTUfKf3auOgOob7l pelleo@PELLEOPC'
 
 
-az vm create 
-  -l ${location} \
-  -g ${resourceGroupName} \
-  -name ${vmName} \
-  --image ${publisher}:${offer}:${sku}:${version} \
-  --size ${vmSize} \
-  --admin-username ${linuxAdminUsername} \
-  --authentication-type ssh \
-  --ssh-key-values ${sshRSAPublicKey} \
-  --nics ${networkInterfaceName} \
-  --custom-data cloud-init-test.txt
+az vm create -l ${location} \
+             -g ${resourceGroupName} \
+             -name ${vmName} \
+             --image ${publisher}:${offer}:${sku}:${version} \
+             --size ${vmSize} \
+             --admin-username ${linuxAdminUsername} \
+             --authentication-type ssh \
+             --ssh-key-values ${sshRSAPublicKey} \
+             --nics ${networkInterfaceName} \
+             --custom-data cloud-init-test.txt
