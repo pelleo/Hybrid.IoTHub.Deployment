22ca7c72f6ab25cae8bbf31699f23ab2f8e07e9b
Pelle Olsson <pelleo@microsoft.com>
2021-12-10 21:09:13 +0100

    Updated cloud-init script

--- deployment/bicep/modules/create_cloud_init_input_string_bicep.sh
+++ deployment/bicep/modules/create_cloud_init_input_string_bicep.sh
@@ -29,11 +29,11 @@ runcmd:
   - mkdir -p /home/${LINUX_ADMIN_USERNAME}/.kube
   - cp /etc/rancher/k3s/k3s.yaml /home/${LINUX_ADMIN_USERNAME}/.kube/config
   - kubectl create ns ${ARGOCD_NAMESPACE}
-  - helm upgrade --kubeconfig /etc/rancher/k3s/k3s.yaml --install demo-argo-cd argo/argo-cd --version ${ARGOCD_VERSION} -n ${ARGOCD_NAMESPACE}
+  - helm upgrade --kubeconfig /etc/rancher/k3s/k3s.yaml --install ${ARGOCD_RELEASE_NAME} argo/argo-cd --version ${ARGOCD_VERSION} -n ${ARGOCD_NAMESPACE}
   - rm -rf /home/${LINUX_ADMIN_USERNAME}/linux-amd64
   - chown -R ${LINUX_ADMIN_USERNAME}:${LINUX_ADMIN_USERNAME} /home/${LINUX_ADMIN_USERNAME}/.kube
   - chown ${LINUX_ADMIN_USERNAME}:${LINUX_ADMIN_USERNAME} /home/${LINUX_ADMIN_USERNAME}/${HELM_TAR_BALL}
-  - curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+  - curl -sSL -o /usr/local/bin/argocd ${ARGOCD_INSTALL_URL}
   - chmod 755 /usr/local/bin/argocd
 final_message: >
     Open a web browser to access ArgoCD at http://${HOST_IP_ADDRESS_OR_FQDN}
@@ -42,4 +42,3 @@ EOSTR
 
 # Double quotes around CLOUD_INIT_STR preserve newlines in stdout.  Required by cloud-init.
 echo "${CLOUD_INIT_STR}" | base64 -w 0 | awk '{printf "{\"cloudInitFileAsBase64\": \"%s\"}", $1}' > ${AZ_SCRIPTS_OUTPUT_PATH}
-#cat ${AZ_SCRIPTS_OUTPUT_PATH}/scriptoutput-v2.json | jq -r '.cloudInitFileAsBase64' | base64 -d
