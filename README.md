f188c53666b276266aba262f663855b4807783e8
Pelle Olsson <pelleo@microsoft.com>
2021-12-12 15:23:41 +0100

    Promoted clount-init script URI to pipeline env var.  Misc code changes to Argo CD script

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Hybrid.IoTHub.Deployment
 This repository contains the necessary code to bring up two K8s environments that can be used to simulate a typical deployment scenario for IoTHub OPC Publisher devtest cycles.  The first environment is used to simulate a "permanent" production cluster:
 
-* Ubuntu Linux virtual machine hosting an "onpremises" K3s cluster that runs [Argo CD](https://argo-cd.readthedocs.io/en/stable/) to support GitOps workflow
-* Virtual network representing the corporate network
-* Storage account, which can be used to support miscellaneous services such as file shares, etc.
-* Azure resource group: `rg-k3s-demo`
+- Ubuntu Linux virtual machine hosting an "onpremises" K3s cluster that runs [Argo CD](https://argo-cd.readthedocs.io/en/stable/) to support GitOps workflow
+- Virtual network representing the corporate network
+- Storage account, which can be used to support miscellaneous services such as file shares, etc.
+- Azure resource group: `rg-k3s-demo`
 
 All software configuration like installation of the K3s cluster (a single master node), Helm, ArgoCD, etc., is done via [cloud-init](https://cloudinit.readthedocs.io/en/latest/), which makes it possible to apply the same configuration technique - with some minor modifications - across multiple platforms including VMs residing on a corporate LAN or even bare metal installations.
 
 The second environment is an AKS cluster, which is meant to represent an "ephemeral" cloud based devtest environment mimicking the onpremises environment:
 
-* Managed AKS cluster
-* IoTHub and optionally also Device Provisioning Service
-* File services.  Default: `SMB`
-* Azure resource group: `rg-aks-demo`
+- Managed AKS cluster
+- IoTHub and optionally also Device Provisioning Service
+- File services.  Default: `SMB`
+- Azure resource group: `rg-aks-demo`
 
 The Argo CD instance running on the K3s cluster is used to deploy K8s workloads on the AKS as well as the K3s cluster.  Both environments are created via [GitHub Actions](https://docs.github.com/en/actions) automation.
 
--- scripts/configure_argocd.sh
+++ scripts/configure_argocd.sh
@@ -1,38 +1,42 @@
 #/bin/bash
 set -euo pipefail
 
-# Login information for Azure VM hosting ArgoCD service.
+# Login information for Azure VM hosting Argo CD service.
 SERVER=demo-y4sz7dkvnweq4.westeurope.cloudapp.azure.com
 ADMIN_USERNAME=adminuser
 
-# Bash local home directory.
-HOME_DIR=/home/pelleo
-
 # Default local kubeconfig directory.
 KUBECONFIG_DIR=/c/Users/pelleo/.kube
-
-# ArgoCD config.
-ARGOCD_NAMESPACE=argocd
-ARGOCD_ADMIN=admin
-ARGOCD_PWD=P@szw0rd
-
-# Repository information.
-REPO_ROOT=~/trumpf/share/Hybrid.IoTHub.Deployment
-REPO=https://github.com/pelleo/Hybrid.IoTHub.Deployment.git
-APP_PATH=clusters/k3s/guestbook
 
 # AKS cluster info.
 AKS_RG_NAME=rg-aks-demo
 AKS_CLUSTER_NAME=demo-aks
 
+# Argo CD config.
+ARGOCD_NAMESPACE=argocd
+ARGOCD_ADMIN=admin
+ARGOCD_PWD=P@szw0rd
+ARGOCD_APP_PATH=clusters/k3s/guestbook
+
+# Repository information.
+REPO_URL=https://github.com/pelleo/Hybrid.IoTHub.Deployment.git
+REPO_NAME=${REPO_URL##*/}
+REPO_NAME=${REPO_NAME%%.git}
+
+# Get path to current script. Use below syntax rather than SCRIPTPATH=`pwd` 
+# for proper handling of edge cases like spaces and symbolic links.
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+LOCAL_PARENT_DIR=${SCRIPT_PATH%%${REPO_NAME}*}
+LOCAL_REPO_ROOT=${LOCAL_PARENT_DIR}/${REPO_NAME}
+
 # Navigate to script dir.  Download kubeconfig and node token from VM.
-ssh-keygen -f ${HOME_DIR}/.ssh/known_hosts -R ${SERVER}
-scp -o "StrictHostKeyChecking no" ${ADMIN_USERNAME}@${SERVER}:k3s-config ${REPO_ROOT}/local
-scp -o "StrictHostKeyChecking no" ${ADMIN_USERNAME}@${SERVER}:node-token ${REPO_ROOT}/local
+ssh-keygen -f ${HOME}/.ssh/known_hosts -R ${SERVER}
+scp -o "StrictHostKeyChecking no" ${ADMIN_USERNAME}@${SERVER}:k3s-config ${LOCAL_REPO_ROOT}/local
+scp -o "StrictHostKeyChecking no" ${ADMIN_USERNAME}@${SERVER}:node-token ${LOCAL_REPO_ROOT}/local
 
 # WSL fix. Must copy the new kubeconfig to default WSL location.
 mv ${KUBECONFIG_DIR}/config ${KUBECONFIG_DIR}/config.bak           # Backup existing kubeconfig
-cp ${REPO_ROOT}/local/k3s-config ${KUBECONFIG_DIR}/config
+cp ${LOCAL_REPO_ROOT}/local/k3s-config ${KUBECONFIG_DIR}/config
 
 # Merge AKS cluster kubeconfig into default config store.
 az aks get-credentials -g ${AKS_RG_NAME} -n ${AKS_CLUSTER_NAME}
@@ -41,11 +45,12 @@ az aks get-credentials -g ${AKS_RG_NAME} -n ${AKS_CLUSTER_NAME}
 kubectl config get-contexts -o name
 kubectl config use-context default
 
-# Verify ArgoCD deploymemnt.
+# Verify ArgoCD deploymemnt.  Timeout after 10 minutes.
 kubectl -n ${ARGOCD_NAMESPACE} get all
 ARGOCD_SERVER_POD_NAME=$(kubectl get pod -n ${ARGOCD_NAMESPACE} -l app.kubernetes.io/name=argocd-server --output=jsonpath="{.items[*].metadata.name}")
 ARGOCD_SERVER_SVC_NAME=$(kubectl get svc -n ${ARGOCD_NAMESPACE} -l app.kubernetes.io/name=argocd-server --output=jsonpath="{.items[*].metadata.name}")
 kubectl wait --for=condition=Ready --timeout=600s -n ${ARGOCD_NAMESPACE} pod/${ARGOCD_SERVER_POD_NAME}
+sleep 10s
 
 # Retrieve random password generated during ArgoCD installation.
 ARGOCD_AUTO_PWD=$(kubectl -n ${ARGOCD_NAMESPACE} get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
@@ -59,14 +64,14 @@ argocd account update-password --current-password ${ARGOCD_AUTO_PWD} --new-passw
 # Allow direct external access (no port-forwarding required).  MUST INSTALL LOADBALANCER RESOURCE!!!  NodePort will not work in Azure!!!
 #kubectl expose deployment.apps/demo-argo-cd-argocd-server --type="NodePort" --port 8080 --name=argo-nodeport -n argocd  
 #kubectl patch service/demo-argo-cd-argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
-kubectl patch svc/${ARGOCD_SERVER_SVC_NAME} -n ${ARGOCD_NAMESPACE} -p '{"spec": {"type": "LoadBalancer"}}'
+#kubectl patch svc/${ARGOCD_SERVER_SVC_NAME} -n ${ARGOCD_NAMESPACE} -p '{"spec": {"type": "LoadBalancer"}}'
 
 # Install sample application.  New login required since credentials changed.
 argocd login ${SERVER} --password ${ARGOCD_PWD}  --username ${ARGOCD_ADMIN} --insecure
-argocd app create guestbook --repo ${REPO} --path ${APP_PATH} --dest-server https://kubernetes.default.svc --dest-namespace default
+argocd app create guestbook --repo ${REPO_URL} --path ${ARGOCD_APP_PATH} --dest-server https://kubernetes.default.svc --dest-namespace default
 
 # Connect GitHub repo.
-argocd repo add ${REPO}
+argocd repo add ${REPO_URL}
 
 # Add AKS cluster to ArgoCD
 kubectl config get-contexts -o name
