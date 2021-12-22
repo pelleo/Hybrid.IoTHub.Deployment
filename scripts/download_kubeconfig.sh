79215d1b367fe235544cf725e36cfed55a80e897
Pelle Olsson <pelleo@microsoft.com>
2021-12-22 18:45:51 +0100

    Changed local_repo logic

--- scripts/download_kubeconfig.sh
+++ scripts/download_kubeconfig.sh
@@ -16,17 +16,19 @@ aks_cluster_name=demo-aks
 repo_name=Hybrid.IoTHub.Deployment
 
 # Get path to current script. Use below syntax rather than SCRIPTPATH=`pwd` 
-# for proper handling of edge cases like spaces and symbolic links.  Use
-# GITHUB_WORKSPACE variable to determin if script is run locally or via
-# Git Actions. 
+# for proper handling of edge cases like spaces and symbolic links.  
 script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 local_parent_dir=${script_path%%${repo_name}*}
-if [[ -z ${GITHUB_WORKSPACE+x} ]]; then 
-    local_repo_root=${local_parent_dir}/${repo_name}
-else
-    #local_repo_root=/github/workspace
-    local_repo_root=${GITHUB_WORKSPACE} 
-fi
+local_repo_root=${local_parent_dir}/${repo_name}
+#if [[ -z ${GITHUB_WORKSPACE+x} ]]; then 
+#    local_repo_root=${local_parent_dir}/${repo_name}
+#else
+#    #local_repo_root=/github/workspace
+#    local_repo_root=${GITHUB_WORKSPACE} 
+#fi
+
+# local_repo_root == GITHUB_WORKSPACE when using Git Actions.
+[[ -z ${GITHUB_WORKSPACE+x} ]] || local_repo_root=${GITHUB_WORKSPACE} 
 
 # File path of kubeconfig on remote K3s host
 file_path=/home/${admin_username}/k3s-config
